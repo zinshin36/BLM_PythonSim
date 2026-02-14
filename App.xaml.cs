93592b2925d0dcba728c4d96a82450bf0cd28ed6
zinshin36 <leewd52@gmail.com>
2026-02-13 23:20:56 -0500

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,12 +4,5 @@ namespace BLMRotationSim
 {
     public partial class App : Application
     {
-        [System.STAThreadAttribute()]
-        public static void Main()
-        {
-            App app = new App();
-            app.InitializeComponent();
-            app.Run();
-        }
     }
 }
