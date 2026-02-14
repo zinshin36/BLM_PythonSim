db7c61846f0cc4d2945be3d24c96b660d2ea5f91
zinshin36 <leewd52@gmail.com>
2026-02-14 14:44:10 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration constants
+ILVL_WINDOW_DEFAULT = 30
+PRIORITY_MODES = ["Crit Focus", "Spell Speed Focus", "Balanced"]
+THEMES = ["Light", "Dark"]
+JOB_CATEGORY = "Caster"
+API_SEARCH_URL = "https://xivapi.com/search"
+API_ITEM_URL = "https://xivapi.com/item"
+MAX_API_RESULTS = 500
