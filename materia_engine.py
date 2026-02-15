bd1fed1f4b61f4fa855a0026c5f6a0b94c26d372
zinshin36 <leewd52@gmail.com>
2026-02-14 20:44:45 -0500

    Delete materia_engine.py

--- materia_engine.py
+++
@@ -1,9 +0,0 @@
-def apply_materia(gear_piece, priority_list):
-    slots = gear_piece.get("MateriaSlots",0)
-    gear_stats = gear_piece["Stats"]
-    # Simplified placeholder logic: evenly distribute by priority
-    for stat in priority_list:
-        if slots > 0:
-            gear_stats[stat] += 10  # Placeholder value per materia
-            slots -=1
-    return gear_piece
