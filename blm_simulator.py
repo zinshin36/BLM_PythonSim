13790f6c71f43b7cdf245d4583644de4371ddd20
zinshin36 <leewd52@gmail.com>
2026-02-27 10:42:53 -0500

    Update blm_simulator.py

--- blm_simulator.py
+++ blm_simulator.py
@@ -1,40 +1,22 @@
-CURRENT_EXPANSION = "Dawntrail"
-
-EXPANSION_CONSTANTS = {
-    "Dawntrail": {
-        "BASE_MAIN": 390,
-        "BASE_SUB": 400,
-        "BASE_DIV": 1900
-    }
-}
-
-
-def set_expansion(name):
-    global CURRENT_EXPANSION
-    CURRENT_EXPANSION = name
-
-
-def simulate_blm_dps(gear_set):
-    const = EXPANSION_CONSTANTS.get(
-        CURRENT_EXPANSION,
-        EXPANSION_CONSTANTS["Dawntrail"]
-    )
-
-    BASE_MAIN = const["BASE_MAIN"]
-    BASE_SUB = const["BASE_SUB"]
-    BASE_DIV = const["BASE_DIV"]
-
+def simulate_dps(gear_set):
     total = {}
+
     for item in gear_set.values():
         for stat, val in item["stats"].items():
             total[stat] = total.get(stat, 0) + val
 
     main = total.get("Intelligence", 3000)
-    crit = total.get("CriticalHit", BASE_SUB)
-    det = total.get("Determination", BASE_SUB)
+    crit = total.get("CriticalHit", 400)
+    det = total.get("Determination", 400)
+    dh = total.get("DirectHit", 400)
+    ss = total.get("SpellSpeed", 400)
+    wd = total.get("WeaponDamage", 120)
 
-    main_mod = 1 + (main - BASE_MAIN) / BASE_DIV
-    det_mod = 1 + (det - BASE_MAIN) / BASE_DIV
-    crit_mod = 1 + (crit - BASE_SUB) / BASE_DIV
+    crit_mod = 1 + ((crit - 400) / 1900)
+    det_mod = 1 + ((det - 400) / 1900)
+    dh_mod = 1 + ((dh - 400) / 3300)
+    ss_casts = 60 / (2.5 - ((ss - 400) / 1300))
 
-    return main_mod * det_mod * crit_mod * 100000
+    potency = 320
+
+    return wd * main * crit_mod * det_mod * dh_mod * potency * ss_casts
