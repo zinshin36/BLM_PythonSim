096ceb45c1bacc432bd1556e792432c0a8b3b35f
zinshin36 <leewd52@gmail.com>
2026-03-12 04:15:50 -0400

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,21 +1,27 @@
 from itertools import product
+from heapq import nlargest
+
 from blm_simulator import simulate_dps
 from materia_engine import apply_optimal_materia
 
 
-def find_best_set(slots, blacklist):
+def find_best_sets(slots, blacklist, top_n=200):
+
     slot_keys = list(slots.keys())
-    best_score = -1
-    best_set = None
+
+    results = []
 
     for combo in product(*(slots[k] for k in slot_keys)):
+
         gear_set = {}
         valid = True
 
         for i, item in enumerate(combo):
+
             if item["name"] in blacklist:
                 valid = False
                 break
+
             gear_set[slot_keys[i]] = dict(item)
 
         if not valid:
@@ -26,8 +32,8 @@ def find_best_set(slots, blacklist):
 
         score = simulate_dps(gear_set)
 
-        if score > best_score:
-            best_score = score
-            best_set = gear_set
+        results.append((score, gear_set))
 
-    return best_set, best_score
+    best = nlargest(top_n, results, key=lambda x: x[0])
+
+    return best
