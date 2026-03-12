b3bdd67c9e64e02af3dd191a65d97454a7277819
zinshin36 <leewd52@gmail.com>
2026-03-12 04:21:51 -0400

    Update solver_engine.py

--- solver_engine.py
+++ solver_engine.py
@@ -1,9 +1,9 @@
 import logging
 from data_models import Spell, PlayerState
+from config import FIGHT_DURATION
 
 class BLMSolver:
-
-    def __init__(self, fight_duration: float):
+    def __init__(self, fight_duration=FIGHT_DURATION):
         self.fight_duration = fight_duration
         self.state = PlayerState()
         self.rotation_log = []
@@ -18,17 +18,13 @@ class BLMSolver:
     def cast_spell(self, spell: Spell):
         self.state.time_elapsed += spell.cast_time
         self.state.mp -= spell.mp_cost
-
-        if self.state.mp <= 0:
+        if self.state.mp < 0:
             self.state.mp = 0
-
         self.log(f"{round(self.state.time_elapsed,2)}s: Cast {spell.name} | MP: {self.state.mp}")
 
     def simulate(self):
         self.log("Starting simulation")
-
         while self.state.time_elapsed < self.fight_duration:
-
             if self.state.mp > 3000:
                 self.cast_spell(self.fire)
                 self.state.in_astral_fire = True
@@ -37,7 +33,6 @@ class BLMSolver:
                 self.cast_spell(self.blizzard)
                 self.state.in_astral_fire = False
                 self.state.in_umbral_ice = True
-                self.state.mp = 10000  # simulate MP refresh in ice
-
+                self.state.mp = 10000
         self.log("Simulation complete")
         return self.rotation_log
